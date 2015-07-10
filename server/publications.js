3e6fa1fb14463dc63aa16c4bfb2b1d64c671cec7
therrealdude <dandersonerling@gmail.com>
2015-07-09 20:43:13 -0500

    Refactored postsLists route into a RouteController.

--- server/publications.js
+++ server/publications.js
@@ -1,7 +1,10 @@
-Meteor.publish('posts', function() {
-  return Posts.find();
+Meteor.publish('posts', function(options) {
+  check(options, {
+    sort: Object,
+    limit: Number
+  });
+  return Posts.find({}, options);
 });
-
 Meteor.publish('comments', function(postId) {
   check(postId, String);
   return Comments.find({postId: postId});
