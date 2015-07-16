3a68202bc4adba59cc1e52fe32840b38a9cc69d4
Daniel Anderson <dandersonerling@gmail.com>
2015-07-16 17:21:54 -0500

    trying to associate images with a user

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -1,3 +1,6 @@
+var images = undefined;
+var imageOptions = undefined;
+
 Template.postSubmit.onCreated(function() {
   Session.set('postSubmitErrors', {});
 });
@@ -17,17 +20,17 @@ Template.postSubmit.helpers({
 Template.postSubmit.events({
   'submit form': function(e) {
     e.preventDefault();
-    
+	
     var post = {
       url: $(e.target).find('[name=url]').val(),
       title: $(e.target).find('[name=title]').val()
     };
-      
+	
     var errors = validatePost(post);
     if (errors.title || errors.url)
       return Session.set('postSubmitErrors', errors);
 
-    Meteor.call('postInsert', post, function(error, result) {
+    Meteor.call('postInsert', post, images, imageOptions, function(error, result) {
       // display the error to the user and abort
       if (error)
         Errors.throw(error.reason);
@@ -38,5 +41,16 @@ Template.postSubmit.events({
     
       Router.go('newPosts');  
     });
+  },
+  'change input[type=file].upload': function(e, helper){
+	  var options = {context:this};
+	  if(helper.data && _.has(helper.data,"callback")){
+			options.callback = helper.data.callback;
+		}
+	  if(helper.data && _.has(helper.data,"public_id")){
+			options.public_id = helper.data.public_id;
+		}
+	  imageOptions = options;
+	  images = e.currentTarget.files;
   }
 });
