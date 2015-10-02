d131f412786bfc284a9d593ecc0bc898b1d5d2ea
therrealdude <dandersonerling@gmail.com>
2015-10-02 01:24:13 -0500

    worked on images

--- client/templates/images/imageFeature.js
+++ client/templates/images/imageFeature.js
@@ -2,9 +2,13 @@ Template.imageFeature.helpers({
     featuredImage: function(){
         for(var i = 0; i< this.images.length; i++){
             if(this.images[i].primary){
-                console.log(this.images[i].response.public_id);
+                Session.set('hasFeaturedImage' + this._id, true);
                 return this.images[i].response;
             }
         }
+        Session.set('hasFeaturedImage' + this._id, false);
+    },
+    hasFeaturedImage: function(){
+        return Session.get('hasFeaturedImage' + this._id);
     }
 });
--- client/templates/images/imageUpload.js
+++ client/templates/images/imageUpload.js
@@ -7,14 +7,39 @@ Template.imageUpload.onCreated(function(){
     }
 });
 
+Template.imageUpload.onRendered(function(){
+    $('#imageDiv').slimScroll({
+        'height': '400px'
+    });
+});
+
 Template.imageUpload.helpers({
     images: function(){
-        return Cloudinary.collection
+        var images = Cloudinary.collection
             .find({status: 'complete'})
             .fetch()
             .map(function(i){
             return i.response;
         });
+        var ret = [];
+        for (var i = 0; i<images.length; i+=3)
+        {
+            var image1 = null;
+            var image2 = null;
+            var image3 = null;
+            
+            if(i< images.length){
+                image1 = images[i];
+            }
+            if(i+1 < images.length){
+                image2 = images[i + 1];
+            }
+            if (i+2 < images.length){
+                image3 = images[i + 2];
+            }
+            ret.push({image1: image1, image2: image2, image3: image3});
+        }
+        return ret;
     },
     hasImages: function(){
         return Cloudinary.collection.find().fetch().length > 0;
@@ -28,9 +53,12 @@ Template.imageUpload.events({
     'change [name=imageUpload]': function(e){
         Cloudinary.upload(e.currentTarget.files);
         e.currentTarget.files = [];
-    },
+    }
+});
+
+Template.imageSelectionItem.events({
     'change [name=primary]': function(e){
-        Cloudinary.collection.update({}, {$set: {primary: false}});
+        Cloudinary.collection.update({}, {$set: {primary: false}}, {multi: true});
         Cloudinary.collection.update({'response.public_id': $(e.target).attr('id')}, {$set: {primary: $(e.target).prop('checked')}});
     }
 });
